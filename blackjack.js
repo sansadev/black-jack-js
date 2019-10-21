49bbf4de9aaf22b1f2f92879a84088b0366342aa
swati <swatidash2007@gmail.com>
2019-10-21 14:23:52 -0700

    created a js file to start blackjack app

---
+++ blackjack.js
@@ -0,0 +1,14 @@
+//blackjack
+//by Swati
+
+
+
+let card1="Ace of spades";
+card2="Ten of Hearts";
+
+console.log("Welcome to Blackjack!");
+
+
+console.log("You are Dealt: ");
+console.log(" " + card1);
+console.log(" " + card2);
